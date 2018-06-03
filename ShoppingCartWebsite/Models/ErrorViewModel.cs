cc50e21386a161d70be0231dff87bcd99243f73e
Aman Baloch <updateaman@live.com>
2018-06-03 07:19:49 -0700

    Added website

---
+++ ShoppingCartWebsite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ShoppingCartWebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
